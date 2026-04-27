b2d8ec41048034e7e5fb7b9ee1e61f4e3229224c
Dmat39 <djcord2900@gmail.com>
2026-04-27 09:24:14 -0500

    feat(user): gestión de usuarios y DTOs

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  lastname: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  dni?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+}
